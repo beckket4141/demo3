7b886c84483a5b4795a005fee9189645768983d7
Beckket4141 <a13667401861@126.com>
2025-07-15 22:28:47 +0800

    2025.7.15

--- 1.py
+++ 1.py
@@ -1,5 +1,14 @@
-print('hello world')
+path=[1,2,3]
+dest=4
+print(f"find path:{'->'.join(map(str,path))}->{dest}")
 
-print('hello world')
-print('hello world')
-print('hello world')
+#demo
+pathstr=['1','2','3']
+result=list(map(int,pathstr))
+print(result)  # 输出：[1, 2, 3]
+
+dictstr=['a:1','b:2']
+result=[dict([item.split(':')]) for item in dictstr]
+print(result)  # 输出: [{'a': '1'}, {'b': '2'}]
+
+print(f"{'...'.join(pathstr)}")
